093b15c08abc305d8bf5f1f579e836973ad20faa
Eric Sondergard <4650644+esond@users.noreply.github.com>
2023-12-11 14:53:53 -0600

    Add blazor web app from template

---
+++ src/Server/Data/ApplicationUser.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Identity;
+
+namespace BlazorIdentity.Web.Server.Data;
+
+// Add profile data for application users by adding properties to the ApplicationUser class
+public class ApplicationUser : IdentityUser
+{
+}
+
