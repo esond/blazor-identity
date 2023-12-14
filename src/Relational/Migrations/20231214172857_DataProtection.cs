e2a0ccbb00316108dd8c1e0f3f1abb874f04102e
Eric Sondergard <4650644+esond@users.noreply.github.com>
2023-12-14 12:26:04 -0600

    Fix formatting

--- src/Relational/Migrations/20231214172857_DataProtection.cs
+++ src/Relational/Migrations/20231214172857_DataProtection.cs
@@ -1,35 +1,34 @@
-﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
 
-namespace BlazorIdentity.Web.Server.Migrations
+namespace BlazorIdentity.Web.Server.Migrations;
+
+/// <inheritdoc />
+public partial class DataProtection : Migration
 {
     /// <inheritdoc />
-    public partial class DataProtection : Migration
+    protected override void Up(MigrationBuilder migrationBuilder)
     {
-        /// <inheritdoc />
-        protected override void Up(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.CreateTable(
-                name: "DataProtectionKeys",
-                columns: table => new
-                {
-                    Id = table.Column<int>(type: "int", nullable: false)
-                        .Annotation("SqlServer:Identity", "1, 1"),
-                    FriendlyName = table.Column<string>(type: "nvarchar(max)", nullable: true),
-                    Xml = table.Column<string>(type: "nvarchar(max)", nullable: true)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_DataProtectionKeys", x => x.Id);
-                });
-        }
+        migrationBuilder.CreateTable(
+            name: "DataProtectionKeys",
+            columns: table => new
+            {
+                Id = table.Column<int>(type: "int", nullable: false)
+                    .Annotation("SqlServer:Identity", "1, 1"),
+                FriendlyName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                Xml = table.Column<string>(type: "nvarchar(max)", nullable: true)
+            },
+            constraints: table =>
+            {
+                table.PrimaryKey("PK_DataProtectionKeys", x => x.Id);
+            });
+    }
 
-        /// <inheritdoc />
-        protected override void Down(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.DropTable(
-                name: "DataProtectionKeys");
-        }
+    /// <inheritdoc />
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.DropTable(
+            name: "DataProtectionKeys");
     }
 }
